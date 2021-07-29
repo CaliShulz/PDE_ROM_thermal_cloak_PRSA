d67675e6f44f38b241ba628eacd15a1438c8b60b
Calishulz <43503918+CaliShulz@users.noreply.github.com>
2021-07-30 00:58:21 +0200

    upgraded gitignore

--- run_all.m
+++ run_all.m
@@ -15,25 +15,25 @@ for tt = 1:length(mesh_names)
    
    mesh_name = erase(mesh_names(tt).name,'.mat');
    
-%    assemblingStart = tic();
-%    assemble_problem(mesh_name);
-%    assembling_time(tt) = toc(assemblingStart);
-%    
-%    SSreductionStart = tic();
-%    SS_reduction(mesh_name);
-%    SS_ocp_time(tt) = toc(SSreductionStart);
-%    
-%    trainingStart = tic();
-%    T_POD_training(mesh_name);
-%    T_POD_training_time(tt) = toc(trainingStart);
-%    
-%    T_reductionStart = tic();
-%    T_reduction(mesh_name);
-%    T_reduction_time(tt) = toc(T_reductionStart);
-%    
-%    T_reduction_testsStart = tic();
-%    T_reduction_tests(mesh_name);
-%    T_reduction_tests_time(tt) = toc(T_reduction_testsStart);
+    assemblingStart = tic();
+    assemble_problem(mesh_name);
+    assembling_time(tt) = toc(assemblingStart);
+    
+    SSreductionStart = tic();
+    SS_reduction(mesh_name);
+    SS_ocp_time(tt) = toc(SSreductionStart);
+   
+   trainingStart = tic();
+   T_POD_training(mesh_name);
+   T_POD_training_time(tt) = toc(trainingStart);
+   
+   T_reductionStart = tic();
+   T_reduction(mesh_name);
+   T_reduction_time(tt) = toc(T_reductionStart);
+   
+   T_reduction_testsStart = tic();
+   T_reduction_tests(mesh_name);
+   T_reduction_tests_time(tt) = toc(T_reduction_testsStart);
    
    T_plotsStart = tic();
    T_plots(mesh_name);
